24bf240c0daf17c4939850097cf43c5ae12a4581
Mike Blemberg <mikeblemberg@gmail.com>
2020-04-23 15:04:54 -0500

    wrote function to generate list of primes below n

--- pe10.py
+++ pe10.py
@@ -12,7 +12,16 @@ Need a faster primality test for this one.  Also, when incrementing to find the 
 prime, increment by 2, not one, since primes above 2 can only be odd.
 '''
 
+import array
+import math
+
 def isPrime(num):
+    ''' use a layered approach.
+        1. is it a multiple of a known prime? -> composite
+        2. does it fail Fermat's test? -> composite
+        3. finally, run the trial division test to determine primality
+    '''
+
     if isPrimeFermat(num) and isPrimeTrial(num):
         return True
     else:
@@ -45,17 +54,37 @@ def isPrimeTrial(num):
     else:
         return True
 
-def nextPrimeAbove(num):
-    while True:
-        num += 2
-        if isPrimeFermat(num):
-            return num
+def primesBelow(num):
+    #use a sieve of Eratosthenes to genearte an array of all primes below num
+
+    list = range(0, num)
+    isPrime = [1]*len(list)
+    isPrime[0] = isPrime[1] = 0
+
+    for i in range(2, math.ceil(math.sqrt(num))):
+        if isPrime[i]:
+            for j in range(i + 1, num):
+                if j % i == 0:
+                    isPrime[j] = 0
+    
+    listOfPrimes = []
+
+    for i in range(len(isPrime)):
+        if isPrime[i]:
+            listOfPrimes.append(list[i])
+
+    return listOfPrimes
 
 n = 7
 sum = 17
 
+
 while n < int(2E6):
-    n = nextPrimeAbove(n)
-    sum += n
+
+    n += 2
+
+    if isPrime(n):
+        sum += n
+
 else:
     print(f'The sum of all primes below two million is {sum}')
